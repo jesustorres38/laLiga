e1bb932e3882f743d638101c9077e486638564de
Jesus Torres <jesustorres38@hotmail.com>
2017-10-29 20:06:30 +0100

    navbar ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { DogsComponent } from './dogs/dogs.component';
+import { DogProfileComponent } from './dog-profile/dog-profile.component';
+import { ContactComponent } from './contact/contact.component';
+import { AdminComponent } from './admin/admin.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    HomeComponent,
+    LoginComponent,
+    DogsComponent,
+    DogProfileComponent,
+    ContactComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'inicio', component: HomeComponent },
+      {path:'navbar', component: NavbarComponent},
+      {path:'contacto', component: ContactComponent},
+      {path:'perros', component: DogsComponent},
+      {path:'perros/:id', component: DogProfileComponent},
+      {path:'login', component: LoginComponent},
+      {path:'administrar', component: AdminComponent},
+      {path:'', redirectTo:'inicio', pathMatch: 'full' },
+      {path:'**', component: NotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dog-profile/dog-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dog-profile',
+  templateUrl: './dog-profile.component.html',
+  styleUrls: ['./dog-profile.component.css']
+})
+export class DogProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
