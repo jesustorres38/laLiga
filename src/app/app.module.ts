688b02695c0278b9af38d9c232f9892a1206d92f
Jesus Torres <jesustorres38@hotmail.com>
2017-11-09 18:16:14 +0100

    bootstrap remplace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+//Angular Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+//Main component
 import { AppComponent } from './app.component';
 
+//Components of the web
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
@@ -12,6 +15,19 @@ import { DogsComponent } from './dogs/dogs.component';
 import { DogProfileComponent } from './dog-profile/dog-profile.component';
 import { ContactComponent } from './contact/contact.component';
 import { AdminComponent } from './admin/admin.component';
+
+//Firebase Connection
+import * as firebase from 'firebase'; 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCz8_mPHqlxnBrxTNfW2aIpaFAn8U1Sgm8",
+  authDomain: "laliga-b0222.firebaseapp.com",
+  databaseURL: "https://laliga-b0222.firebaseio.com",
+  projectId: "laliga-b0222",
+  storageBucket: "laliga-b0222.appspot.com",
+  messagingSenderId: "554339149815"
+};
+firebase.initializeApp(config);
 
 @NgModule({
   declarations: [
